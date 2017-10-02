da2acd7c074e3e5232820d7b03da3d8e5c17ae3d
gaya <gaya@192.168.31.252>
2017-10-02 15:55:38 +0800

    good

--- src/selenium/GayaSelenium.java
+++ src/selenium/GayaSelenium.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
@@ -13,6 +14,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GayaSelenium {
@@ -30,7 +32,7 @@ public class GayaSelenium {
 		WebDriverWait wait = new WebDriverWait(driver, waitTime);
 		wait.until(ExpectedConditions.elementToBeClickable(locator));
 		WebElement e = driver.findElement(locator);
-		action.doubleClick(e);
+		action.doubleClick(e).perform();
 	}
 	//输入
 	public void sendkeys(By locator,String string){
@@ -62,18 +64,38 @@ public class GayaSelenium {
 	//js点击
 	public void jsClick(By locator){
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebDriverWait wait = new WebDriverWait(driver, waitTime);
 		wait.until(ExpectedConditions.elementToBeClickable(locator));
 		js.executeScript("arguments[0].click();", new Object[]{driver.findElement(locator)});
 	}
 	//等待到达
 	public void waitLocated(By locator){
-		WebDriverWait wait = new WebDriverWait(driver,30);
+		WebDriverWait wait = new WebDriverWait(driver,waitTime);
 		wait.until(ExpectedConditions.presenceOfElementLocated(locator));
 	}
 	//等待标题
 	public void waitTitles(String title){
-		WebDriverWait wait = new WebDriverWait(driver,30);
+		WebDriverWait wait = new WebDriverWait(driver,waitTime);
 		wait.until(ExpectedConditions.titleIs(title));
+	}
+	//alert事件
+	public String alert(int i,String sendkeys){
+		Alert alert = driver.switchTo().alert();
+		switch(i){
+		case 0:
+				alert.accept();
+				break;
+		case 1: 
+				String s =alert.getText();
+				return s;
+		case 2:
+				alert.dismiss();
+				break;
+		case 3:
+				alert.sendKeys(sendkeys);
+				break;
+		}
+		return null;
+	
 	}
 }
---
+++ src/selenium/SeleniumTest.java
@@ -0,0 +1,113 @@
+package selenium;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumTest {
+	static GayaSelenium g = new GayaSelenium();
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/Users/gaya/git/selenium/lib/selenium/chromedriver");
+		g.driver = new ChromeDriver();
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		g.driver.quit();
+	}
+
+	@Test
+	public void selectTest() {
+		g.driver.get("http://localhost/webtest/body.htm");
+		g.click(By.id("a1"));
+		g.click(By.id("a2"));
+		Select sel = new Select(g.driver.findElement(By.name("lesson")));
+		Random random = new Random();
+		int i = random.nextInt(2);
+		System.out.println(i);
+		sel.selectByIndex(i);
+	}
+	@Test
+	public void alertTest(){
+		g.driver.get("http://localhost/webtest/body.htm");
+		g.click(By.id("confirm"));
+//		String s0 = g.driver.switchTo().alert().getText();
+//		g.driver.switchTo().alert().accept();
+//		String s1 = g.driver.switchTo().alert().getText();
+//		g.driver.switchTo().alert().accept();
+//		Alert alert = g.driver.switchTo().alert();
+//		String s0 = alert.getText();
+//		alert.accept();
+//		String s1 = alert.getText();
+//		alert.accept();
+//		System.out.println(s0+">>>"+s1);
+		//调用封装
+		String s = g.alert(1,"");
+		g.alert(0,"");
+		String s1 = g.alert(1,"");
+		g.alert(2,"");
+		System.out.println(s+","+s1);
+		
+//		g.click(By.xpath("//a"));
+		g.click(By.cssSelector("a"));
+	}
+	@Test
+	public void doubleClick(){
+		g.driver.get("http://localhost/webtest/dblClick.htm");
+		String s1 = g.driver.findElement(By.id("showme")).getAttribute("value");
+		g.doubleClick(By.id("winTitle"));
+		String s2 = g.driver.findElement(By.id("showme")).getAttribute("value");
+		System.out.println(s1+","+s2);
+	}
+	@Test
+	public void prompt(){
+		g.driver.get("http://localhost/webtest/dialog.htm");
+		g.click(By.id("prompt"));
+		g.alert(3, "test");
+		g.alert(0, "");
+		System.out.println(g.driver.getPageSource());
+	}
+	@Test
+	public void upload(){
+		g.driver.get("http://localhost/webtest/input.htm");
+		g.driver.findElement(By.id("id5")).sendKeys("/Users/gaya/Desktop/图片/0.jpg");
+	}
+	@Test
+	public void window(){
+		g.driver.get("http://localhost/webtest/locator.htm");
+		String parent = g.driver.getWindowHandle();
+		g.click(By.linkText("百度"));
+		g.click(By.linkText("计算器"));
+		Set<String> handles = g.driver.getWindowHandles();
+		for(String handle : handles){
+			g.driver.switchTo().window(handle);
+			if(g.driver.getTitle().equals("百度一下，你就知道")){
+				g.sendkeys(By.id("kw"), "good");
+				g.click(By.id("su"));
+			}
+			if(g.driver.getTitle().equals("简单计算器")){
+				g.click(By.id("btn7"));
+				g.click(By.id("times"));
+				g.click(By.id("btn6"));
+				g.click(By.id("equal"));
+				System.out.println(g.driver.findElement(By.name("numScreen")).getAttribute("value"));
+			}
+		}
+		g.driver.switchTo().window(parent);
+		g.click(By.name("continue"));
+	}
+}
